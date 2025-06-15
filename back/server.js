a5c43dc4e644ce1f270b822601ed048ecdf61e94
samshubh08 <samshubham1996@gmail.com>
2025-06-16 01:25:44 +0530

    mongodb updations

--- back/server.js
+++ back/server.js
@@ -24,10 +24,7 @@ app.use(limiter);
 
 app.use(express.json({ limit: '10mb' }));
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
